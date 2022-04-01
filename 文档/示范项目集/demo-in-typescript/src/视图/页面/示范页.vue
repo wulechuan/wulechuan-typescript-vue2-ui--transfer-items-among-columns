49ae5fd6e1a10c2f3e26d99878ab4af2400c1d5a
吴乐川 <wulechuan@live.com>
2022-04-02 06:38:42 +0800

    正在改写为 typescript。

--- 文档/示范项目集/demo-in-typescript/src/视图/页面/示范页.vue
+++ 文档/示范项目集/demo-in-typescript/src/视图/页面/示范页.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="page page--示范页">
-        <Ux双列互换条目></Ux双列互换条目>
+        <wlc双列表数据互换></wlc双列表数据互换>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
-import Ux双列互换条目 from '@wulechuan/vue2-ui--two-columns-exchange-items'
+import wlc双列表数据互换 from '@wulechuan/vue2-ui--two-columns-exchange-items'
 
 @Component({
     components: {
-        Ux双列互换条目,
+        wlc双列表数据互换,
     },
 })
 export default class Page示范页 extends Vue {
