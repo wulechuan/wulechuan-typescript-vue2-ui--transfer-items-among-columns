9b3bf7c7adeb5b90245064b349656d0920695bd5
吴乐川 <wulechuan@live.com>
2022-04-10 01:40:18 +0800

    无关痛痒

--- 文档/示范项目集/demo-in-typescript/src/视图/部件/wlc-双列互换数据之条目-自定义形态.vue
+++ 文档/示范项目集/demo-in-typescript/src/视图/部件/wlc-双列互换数据之条目-自定义形态.vue
@@ -1,15 +1,15 @@
 <template>
-    <span v-if="条目" class="吴乐川-双列互换数据之条目 自定义形态">
+    <span class="吴乐川-双列互换数据之条目 自定义形态">
         <span class="输入项 输入项-勾选项" :class="输入项之样式类名配置">
             <span class="勾选项视觉假体"></span>
-            <input type="checkbox" :checked="_条目.已选中" @click.prevent>
+            <input type="checkbox" :checked="条目_最终采纳值.已选中" @click.prevent>
         </span>
-        <span class="输入项配文">{{ 条目.在界面中的称谓 }}</span>
+        <span class="输入项配文">{{ 条目_最终采纳值.在界面中的称谓 }}</span>
     </span>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
 import type Wlc双列互换数据 from '@wulechuan/vue2-ui--two-columns-exchange-items/源代码/发布的源代码/typescript/wlc-双列互换数据.vue'
 
@@ -22,20 +22,23 @@ export default class Wlc双列互换数据之条目之自定义形态示范 exte
 
 
 
-    private _条目: 范_内用格式之条目 = this.构造临时的条目()
 
 
-
-    @Watch('条目', { immediate: true })
-    当条目变动时 (新值?: 范_内用格式之条目): void {
-        if (!新值) { return }
-        console.log('变动：', 新值.在界面中的称谓, '---- 选：', 新值.已选中)
-        this._条目 = 新值
+    private get 条目_最终采纳值 (): 范_内用格式之条目 {
+        const 外界给出的值 = this.条目
+        if (!外界给出的值) return this.构造临时的条目()
+        console.log('变动：', 外界给出的值.在界面中的称谓, '---- 选：', 外界给出的值.已选中)
+        return 外界给出的值
     }
 
     private get 输入项之样式类名配置 (): 范_界面元素之样式类名之配置 {
-        return { '已选中': this._条目.已选中, '已禁止交互': this._条目.已禁止选择, '未选中': !this._条目.已选中 }
+        const 条目 = this.条目_最终采纳值
+        return { '已选中': 条目.已选中, '已禁止交互': 条目.已禁止选择, '未选中': !条目.已选中 }
     }
+
+
+
+
 
     private 构造临时的条目 (): 范_内用格式之条目 {
         const 称谓: string = Math.random().toFixed(10)
@@ -43,3 +46,10 @@ export default class Wlc双列互换数据之条目之自定义形态示范 exte
     }
 }
 </script>
+
+<style lang="stylus">
+.吴乐川-双列互换数据之条目.自定义形态 {
+    display flex
+    flex-direction row
+}
+</style>
