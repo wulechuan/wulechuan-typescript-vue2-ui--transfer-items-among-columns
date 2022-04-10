97ad7662de4b1b04ebfb8c09481ff016cc34aac0
吴乐川 <wulechuan@live.com>
2022-04-11 04:57:07 +0800

    样式表文件结构优化。

--- 文档/示范项目集/demo-in-typescript/src/main.ts
+++ 文档/示范项目集/demo-in-typescript/src/main.ts
@@ -1,3 +1,6 @@
+import '@wulechuan/vue2-ui--two-columns-exchange-items/源代码/发布的源代码/stylus/仅尺寸与排版与关键行为（亦可单独发行）/默认/0-基础.styl'
+import '@wulechuan/vue2-ui--two-columns-exchange-items/源代码/发布的源代码/stylus/仅着色与装饰（亦可单独发行）/默认/0-基础.styl'
+
 import Vue from 'vue'
 import 应用根 from './视图/应用根.vue'
 
