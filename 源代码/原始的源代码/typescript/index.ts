f27ae053df22bfbc3595ba571c2353b36d3dfbfa
吴乐川 <wulechuan@live.com>
2022-04-15 17:26:45 +0800

    勘误

--- 源代码/原始的源代码/typescript/index.ts
+++ 源代码/原始的源代码/typescript/index.ts
@@ -4,7 +4,7 @@ import * as _核心工具集 from './核心工具集'
 export const 辅助工具集 = _辅助工具集
 export const 核心工具集 = _核心工具集
 
-export * from './wlc-任意两列互换数据之单列.vue'
-export * from './wlc-任意两列互换数据之单列-说明书.vue'
-export * from './wlc-任意两列互换数据之条目视觉根-默认形态.vue'
-export * from './wlc-任意两列互换数据之现成实用的双列.vue'
+export * from './wlc-任意两列互换数据-单列.vue'
+export * from './wlc-任意两列互换数据-单列之说明书.vue'
+export * from './wlc-任意两列互换数据-条目之视觉根-默认形态.vue'
+export * from './wlc-任意两列互换数据-现成实用的双列.vue'
