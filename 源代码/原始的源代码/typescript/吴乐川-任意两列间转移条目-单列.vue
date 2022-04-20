b3e76002156da9ff0ace7f09d1d4ce866f31f702
吴乐川 <wulechuan@live.com>
2022-04-21 02:12:05 +0800

    完整的 sass 。

--- 源代码/原始的源代码/typescript/吴乐川-任意两列间转移条目-单列.vue
+++ 源代码/原始的源代码/typescript/吴乐川-任意两列间转移条目-单列.vue
@@ -900,7 +900,7 @@ export default class Wlc任意两列间转移条目_单列 extends Vue {
 // 故，各 Vue 部件均依赖的 .styl 文件（目前仅《 `0-基础.styl` 》）的加载语句，
 // 既不应写在本 <style> 代码块内，也不应写在各 Vue 部件专属的 .styl 文件中。
 // 否则，非但 css 会有重复内容，且重复加载的 css 内容其相对于其他 css 内容之顺序也不能确保正确。
-// 目前推荐的做法是，始终提醒本工具之使用者在 Vue 的 `main.ts` 或 `main.js` 中加载**两**个 `0-基础.styl` 文件。
+// 目前推荐的做法是，始终提醒本工具之使用者在 Vue 的 `main.ts` 或 `main.js` 中加载 `0-基础.styl` 文件。
 
 // @import '../stylus/仅尺寸与排版与关键行为/默认/1-单列.styl'
 // @import '../stylus/仅着色与装饰/默认/1-单列.styl'
