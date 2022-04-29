d696cd3321cef74cb49167cbe728783cb0a88f9a
吴乐川 <wulechuan@live.com>
2022-04-29 15:44:30 +0800

    命令行脚本改进。

--- 用于研发阶段的命令行工具/PowerShell/Add-添加所有集得源.ps1
+++ 用于研发阶段的命令行工具/PowerShell/Add-添加所有集得源.ps1
@@ -1,5 +1,23 @@
 #
 
+Write-Host
+Write-Host '===== 添加【集得】源： 吴乐川：码云  ========================= '
 git  remote  add  '吴乐川：码云'  'git@gitee.com:nanchang-wulechuan/wulechuan-typescript-vue2-ui--transfer-items-among-columns.git'
+Write-Host
+Write-Host
+
+
+
+Write-Host
+Write-Host '===== 添加【集得】源： 吴乐川：阿里云  ========================= '
 git  remote  add  '吴乐川：阿里云'  'git@code.aliyun.com:wulechuan/wulechuan-typescript-vue2-ui--transfer-items-among-columns.git'
+Write-Host
+Write-Host
+
+
+
+Write-Host
+Write-Host '===== 添加【集得】源： 吴乐川：GitHub  ========================= '
 git  remote  add  '吴乐川：GitHub'  'git@github.com:wulechuan/wulechuan-typescript-vue2-ui--transfer-items-among-columns.git'
+Write-Host
+Write-Host
--- 用于研发阶段的命令行工具/bash/Update-更新所有npm依赖包至最新版本.sh
+++ 用于研发阶段的命令行工具/bash/Update-更新所有npm依赖包至最新版本.sh
@@ -5,11 +5,21 @@
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【产品依赖包】。
+# echo
+# echo
+# echo
+# echo  -e  "\e[0;31m===== npm i    \e[97;41m特定版本\e[0;31m的【产品依赖包】 ============================\e[0m"
+# echo
 # 暂无。
 
 
 
 # 以下均为可采用 latest 版本的【产品依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+echo
+echo
+echo
+echo  -e  "\e[0;31m===== npm i    特定版本的【产品依赖包】 ============================\e[0m"
+echo
 npm  i 'vue-property-decorator@latest'
 
 
@@ -21,11 +31,21 @@ npm  i 'vue-property-decorator@latest'
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【研发依赖包】。
+# echo
+# echo
+# echo
+# echo  -e  "\e[32m===== npm i    \e[90;102m特定版本\e[0;32m的【研发依赖包】 ============================\e[0m"
+# echo
 # 暂无。
 
 
 
 # 以下均为可采用 latest 版本的【研发依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+echo
+echo
+echo
+echo  -e  "\e[32m===== npm i    特定版本的【研发依赖包】 ============================\e[0m"
+echo
 npm  i  -D \
     "@typescript-eslint/eslint-plugin@latest" \
     "@typescript-eslint/parser@latest" \
